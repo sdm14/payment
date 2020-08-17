0acd3817b8e9e26c4ab42d0f00e03ee95b1fbf45
sdm14 <149sdm10@gmail.com>
2020-08-18 02:33:36 +0600

    Updates

---
+++ precache-manifest.2e5bd302495251139854ff719b761a8f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d208035608532c11ea1276335286bbb",
+    "url": "/payment/index.html"
+  },
+  {
+    "revision": "8b37623045a1301a4404",
+    "url": "/payment/static/css/main.bb2c47c4.chunk.css"
+  },
+  {
+    "revision": "4d97c8a528147bf6062c",
+    "url": "/payment/static/js/2.4b779f0d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/payment/static/js/2.4b779f0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b37623045a1301a4404",
+    "url": "/payment/static/js/main.6e2ab771.chunk.js"
+  },
+  {
+    "revision": "678da6088e1adc268e71",
+    "url": "/payment/static/js/runtime-main.f803e2f5.js"
+  },
+  {
+    "revision": "0a5f4cb974530c1e239bf1aba209df5f",
+    "url": "/payment/static/media/backgr.0a5f4cb9.png"
+  },
+  {
+    "revision": "dff579a3f508bb56f5630c3365b06888",
+    "url": "/payment/static/media/mts.dff579a3.png"
+  }
+]);
